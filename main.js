5fafc428b8155b5973941ea53d060680888c29f1
AdamG <adamgonlag@gmail.com>
2021-05-02 18:44:47 +1000

    basic version of displayGameBoard function

--- main.js
+++ main.js
@@ -14,6 +14,52 @@
 //              - add classes for style.
 //            }
 
+// ToDo: function to generate gameBoard based on gridsize
+let gameBoard = [1,0,0,1,0,0,1,0,0]
+let gridSize = 3;
+
+// Player 1  always starts first
+let whoseTurn = 1;
+
+function displayGameBoard(gameBoard) {
+
+  // Delete previous version of gameboard if exists
+  let ticTacToe = document.querySelectorAll('#tic-tac-toe')
+  if (ticTacToe) {
+    ticTacToe.parentNode.removeChild(ticTacToe);
+
+  }
+
+  // Create parent div
+  const parentDiv = document.createElement('main')
+  parentDiv.id = 'tic-tac-toe'
+
+  // Add grid styles - TO DO: create function to generate strings based on grid size
+  parentDiv.style.gridTemplateColumns = "200px 200px 200px"
+  parentDiv.style.gridTemplateRows = "200px 200px 200px"
+  
+  // Add to body 
+  document.body.appendChild(parentDiv)
+
+
+  // Add squares and values to the grid based on the current gameboard
+  for (let i = 0; i < gameBoard.length; i++) {
+      const newSquare = document.createElement('div')
+      newSquare.id = `${i}`
+      const squareStatus = gameBoard[i] 
+      if (squareStatus === 1) {
+        newSquare.innerHTML = `X`
+      } else if (squareStatus === 2) {
+        newSquare.innerHTML = `O`
+      } 
+      parentDiv.appendChild(newSquare)
+
+    }
+}
+
+displayGameBoard(gameBoard)
+
+
 // Taking a Turn:
 //    On click of a square:
 //        function takeATurn(whoseTurn) {
@@ -24,6 +70,30 @@
 //                - display new board
 //                - check if winner. If no winner update whoseTurn and allow another turn.
 //         }
+
+
+function takeATurn(whoseTurn, gameBoard) {
+  const squares = document.querySelectorAll('#tic-tac-toe div')
+  for (let i = 0 ; i < squares.length; i++) {
+    const squareClicked = squares[i]
+    squareClicked.addEventListener('click', function (e) {
+      let gameBoardValue = gameBoard[parseInt(e.target.id)]
+      
+      // Check if square click is empty, X or O
+      if (gameBoardValue === 0) {
+        gameBoardValue = whoseTurn
+      } else {
+        console.log('Square taken')
+      }
+      displayGameBoard(gameBoard)
+
+      // 
+    })
+  }
+}
+
+takeATurn(whoseTurn, gameBoard)
+
 
 // Check if Winner:
 //    Check if player has 3 in a row - rows, columns, diagonals
